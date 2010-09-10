dba844f8fd84925883a4d0010532855d5085dc68
Diego Berge <devel@navlost.eu>
2010-09-10 21:50:28 +0000

    Improved string extractor for translations

--- po/extract_strings.sh
+++ po/extract_strings.sh
@@ -12,15 +12,26 @@
     exit -1
 }
 
+[ -z "$(which msgmerge)" ] && {
+    echo "Required application msgmerge not found in search path." >&2
+    echo "Please install the application or add it to your \$PATH" >&2
+    exit -1
+}
+
 WDIR=$(dirname $0)
 
-UIFILELIST=$(find ../ -name '*.ui')
+UIFILELIST=$(find $WDIR/../ -name '*.ui')
 extractrc $UIFILELIST >$WDIR/rc.cpp
 
-CPPFILELIST=$(find ../ -name '*.cpp')
+CPPFILELIST=$(find $WDIR/../ -name '*.cpp')
 
 xgettext -C -o $WDIR/plasma_applet_apcups.pot -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 \
         -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 \
         -kki18ncp:1c,2,3 $CPPFILELIST
 
 rm $WDIR/rc.cpp
+
+for f in $WDIR/*.po; do
+    echo -n "Merging $f "
+    msgmerge -U "$f" "$WDIR/plasma_applet_apcups.pot"
+done
